a6625984d408f2013f6dc943465d11f6c0b3a6e2
itedya <bdomanski@protonmail.com>
2023-05-14 21:34:32 +0200

    Add simple example for functions that will work with api

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,34 @@
-import { createApp } from 'vue'
-import './style.css'
+import { createApp, ref } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+const user = ref();
+const token = ref();
+
+const api = axios.create({
+	baseUrl: '', // tutaj url laravela
+});
+
+api.interceptors.request.use((config) => {
+	if (token !== undefined) {
+		config.headers.Authorization = `Bearer ${token.value}`;
+	}
+
+	return config;
+});
+
+// funkcja do loginu
+api.post("/login", {
+	username: "blabla",
+	password: "blablabla"
+})
+	.then(res => {
+		token.value = res.data.token;
+		user.value = res.data.user;
+	});
+
+api.get("/user")
+	.then(res => {
+		user.value = res.data;
+	});
